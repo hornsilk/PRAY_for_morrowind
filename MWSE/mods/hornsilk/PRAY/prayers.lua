e97443e7611e3663e4311e10ec3230f40ce9bf18
hornsilk <hornsilk01@gmail.com>
2023-07-29 08:29:53 -0600

    add a placeholder prayer for testing

---
+++ MWSE/mods/hornsilk/PRAY/prayers.lua
@@ -0,0 +1,13 @@
+
+local this = {}
+
+this.divinePrayers = {
+    name = "Prayer to Mara",
+    id = "prayer_to_mara",
+    handler = "Divine Prayers",
+    skillReq = 10,
+    skill = "divine",
+    effect = "fortify strength 100",
+    description = "Pray to Mara to fortify my strength.",
+    time = 1
+}
