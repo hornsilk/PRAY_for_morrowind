d3576a1a5d4afeadf3437aec326073007ffca46b
hornsilk <hornsilk01@gmail.com>
2023-07-29 21:59:14 -0700

    set an idle animation to play while praying

--- MWSE/mods/hornsilk/PRAY/animation.lua
+++ MWSE/mods/hornsilk/PRAY/animation.lua
@@ -1,15 +1,15 @@
 local this = {}
 
+-- https://mwse.github.io/MWSE/references/animation-groups/
+
 this.divineAnimationBegin = function()
     tes3.setVanityMode{enabled = true, checkVanityDisabled = false}
-	tes3.playAnimation({ reference = tes3.player, group = tes3.animationGroup.idle2})
+	tes3.playAnimation({ reference = tes3.player, group = tes3.animationGroup.idle8})--, lower = tes3.animationGroup.death1})
 end
 
 this.divineAnimationEnd = function()
     tes3.setVanityMode{enabled = false, checkVanityDisabled = false}
     tes3.loadAnimation{reference = tes3.player}
 end
-
-
 
 return this
