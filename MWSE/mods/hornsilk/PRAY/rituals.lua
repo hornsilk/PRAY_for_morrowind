2ae3d99ec3e6f707a4310545e19ac5b3fffcb266
hornsilk <hornsilk01@gmail.com>
2023-08-02 14:59:23 -0700

    complete tribunal rituals

--- MWSE/mods/hornsilk/PRAY/rituals.lua
+++ MWSE/mods/hornsilk/PRAY/rituals.lua
@@ -347,7 +347,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Aralor",
         image = "Icons\\PRAY\\art\\tx_saint_aralor_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -362,15 +362,15 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn self-respect and respect for others, follow Saint Aralor the Penitent, Patron of Tanners and Miners. This foul criminal repented his sins and traveled a circuit of the great pilgrimages on his knees."
     },
     {
-        name = "Rite of Saint Deyln's Shield",
+        name = "Rite of Saint Delyn's Shield",
         id = "delyn_ritual",
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Delyn",
         image = "Icons\\PRAY\\art\\tx_saint_deyln_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -384,7 +384,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn benevolence, follow Saint Delyn the Wise, Patron of Potters and Glassmakers. Saint Delyn was head of House Indoril, a skilled lawyer, and author of many learned treatises on Tribunal law and custom."
     },
     {
         name = "Rite of Saint Felms's Glory",
@@ -392,7 +392,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Felms",
         image = "Icons\\PRAY\\art\\tx_saint_felms_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -407,7 +407,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn fierce justice, follow Saint Felms the Bold, Patron of Butchers and Fishmongers. This brave warlord slew the Nord invaders and drove them from our lands. He could neither read nor write, receiving inspiration directly from the lips of Almsivi."
     },
     {
         name = "Rite of Saint Llothis's Rock",
@@ -415,7 +415,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Llothis",
         image = "Icons\\PRAY\\art\\tx_saint_llothis_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -430,7 +430,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn reverence, follow Saint Llothis the Pious, Patron of Tailors and Dyers. Contemporary and companion of the Tribunal, and the best-loved Alma Rula of the Tribunal Temple, he formulated the central rituals and principles of the New Temple Faith. Saint Llothis is the symbolic mortal bridge between the gods and the faithful, and the archetypal priest."
     },
     {
         name = "Rite of Saint Meris's Warding",
@@ -438,7 +438,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Meris",
         image = "Icons\\PRAY\\art\\tx_saint_meris_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -452,7 +452,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn the love of peace, follow Saint Meris the Peacemaker, Patron of Farmers and Laborers. As a little girl, Saint Meris showed healing gifts, and trained as a Healer. She ended a long and bloody House War, intervening on the battlefield in her white robe to heal warriors and spellcrafters without regard to faction."
     },
     {
         name = "Rite of Saint Nerevar's Spirit",
@@ -460,7 +460,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Nerevar",
         image = "Icons\\PRAY\\art\\tx_saint_nerevar_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -474,7 +474,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn valor, follow St. Nerevar the Captain, patron of Warriors and Statesmen. Lord Nerevar helped to unite the barbarian Dunmer tribes into a great nation, culminating in his martyrdom when leading the Dunmer to victory against the evil Dwemer and the traitorous House Dagoth in the Battle of Red Mountain."
     },
     {
         name = "Rite of Saint Olms's Benediction",
@@ -482,7 +482,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Olms",
         image = "Icons\\PRAY\\art\\tx_saint_olms_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -496,7 +496,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn the rule of law and justice, follow Saint Olms the Just, Patron of Chandlers and Clerks. Founder of the Ordinators, Saint Olms conceived and articulated the fundamental principles of testing, ordeal, and repentance."
     },
     {
         name = "Rite of Saint Rilms's Grace",
@@ -504,7 +504,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Rilms",
         image = "Icons\\PRAY\\art\\tx_saint_rilms_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -519,7 +519,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn generosity, follow Saint Rilms the Barefooted, Patron of Pilgrims and Beggars. Saint Rilms gave away her shoes, then dressed and appeared as a beggar to better acquaint herself with the poor."
     },
     {
         name = "Rite of Saint Roris's Bloom",
@@ -527,7 +527,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Roris",
         image = "Icons\\PRAY\\art\\tx_saint_roris_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -541,7 +541,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn pride of race and tribe, follow Saint Roris the Martyr, Patron of Furnishers and Caravaners. Captured by Argonians just before the Arnesian War, Roris proudly refused to renounce the Tribunal faith, and withstood the cruel tortures of Argonian sorcerers. Vengeance and justice for the martyred Saint Roris was the rallying cry of the Arnesian War."
     },
     {
         name = "Rite of Saint Seryn's Shield",
@@ -549,7 +549,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Seryn",
         image = "Icons\\PRAY\\art\\tx_saint_seryn_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -563,7 +563,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn mercy and its fruits, follow Saint Seryn the Merciful, Patron of Brewers, Bakers, Distillers. This pure virgin of modest aspect could heal all diseases at the price of taking the disease upon herself. Tough-minded and fearless, she took on the burdens of others, and bore those burdens to an honored old age."
     },
     {
         name = "Rite of Saint Veloth's Indwelling",
@@ -571,7 +571,7 @@ this.tribunalRituals = {
         handler = "Tribunal Rituals",
         skillReq = 20,
         skill = "tribunal_theology",
-        description = "TODO",
+        description = "Purge yourself of disease and study the virtures of Saint Veloth",
         image = "Icons\\PRAY\\art\\tx_saint_veloth_01.dds",
         materials = { { material = "candle", count = 1 } },
         spellEffects = {
@@ -585,7 +585,7 @@ this.tribunalRituals = {
                 id = 69, --cureCommonDisease
             },
         },
-        text = "TODO"
+        text = "If you would learn daring, follow Saint Veloth the Pilgrim, Patron of Outcasts and Spiritual Seekers. Saint Veloth, prophet and mystic, led the Dunmer out of the decadent home country of the Summerset Isles and into the promised land of Morrowind. Saint Veloth also taught the difference between the Good and Bad Daedra, and won the aid of the Good Daedra for his people while teaching how to carefully negotiate with the Bad Daedra."
     },
 }
 
