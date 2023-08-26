289fd0664c1f5a6028082dee57378d007f9bf1c3
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-08-26 08:50:48 +0000

    Disable players controls on pray anim

--- MWSE/mods/hornsilk/PRAY/animation.lua
+++ MWSE/mods/hornsilk/PRAY/animation.lua
@@ -5,10 +5,28 @@ local this = {}
 -- try `the sitting mod` for some better animations
 -- Use tes3.loadAnimation to play custom animations
 
+-- Disable player controls --
+local function setControlsDisabled(state)
+    tes3.mobilePlayer.controlsDisabled = state
+    tes3.mobilePlayer.jumpingDisabled = state
+    tes3.mobilePlayer.attackDisabled = state
+    tes3.mobilePlayer.magicDisabled = state
+    tes3.mobilePlayer.mouseLookDisabled = state
+end
+local function disableControls()
+    setControlsDisabled(true)
+end
+local function enableControls()
+    setControlsDisabled(false)
+    tes3.runLegacyScript{command = "EnableInventoryMenu"}
+end
+
 -- Define a function to begin the default animation
 this.defaultAnimationBegin = function()
     -- Enable vanity mode to allow custom animations
     tes3.setVanityMode{enabled = true, checkVanityDisabled = false}
+
+    disableControls()
 
     -- Play a custom animation (idle8) for the player reference
     tes3.playAnimation({ reference = tes3.player, group = tes3.animationGroup.idle8})
@@ -18,6 +36,8 @@ end
 this.defaultAnimationEnd = function()
     -- Disable vanity mode to revert to normal animations
     tes3.setVanityMode{enabled = false, checkVanityDisabled = false}
+
+    enableControls()
 
     -- Load the default animation for the player reference
     tes3.loadAnimation{reference = tes3.player}
